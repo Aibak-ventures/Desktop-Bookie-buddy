88baf35d7bf6e0921aa3d4bac484a300d266d2df
AFNAN P <116930486+Afnanappu@users.noreply.github.com>
2025-10-15 12:24:01 +0530

    fix: bloc provider not found issue in multiple screens and add network access to macos to run mac app

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -5,19 +5,22 @@ import 'package:bookie_buddy_web/core/view_model/bloc_service/service_bloc.dart'
 import 'package:bookie_buddy_web/core/view_model/bloc_shop_list/shop_list_bloc.dart';
 import 'package:bookie_buddy_web/core/view_model/cubit_booking_selection/booking_selection_cubit.dart';
 import 'package:bookie_buddy_web/core/view_model/cubit_client/client_cubit.dart';
+import 'package:bookie_buddy_web/core/view_model/cubit_product_search/product_search_cubit.dart';
 import 'package:bookie_buddy_web/core/view_model/user_cubit.dart';
 import 'package:bookie_buddy_web/features/add_booking/view_model/bloc_add_booking/add_booking_bloc.dart';
 import 'package:bookie_buddy_web/features/add_booking/view_model/bloc_client_save/client_save_bloc.dart';
+import 'package:bookie_buddy_web/features/add_booking/view_model/cubit/staff_search_cubit.dart';
 import 'package:bookie_buddy_web/features/all_booking/view_model/bloc_all_booking/all_booking_bloc.dart';
 import 'package:bookie_buddy_web/features/auth/view_model/bloc_auth/auth_bloc.dart';
 import 'package:bookie_buddy_web/features/booking_details/view_model/bloc_booking_details/booking_details_bloc.dart';
+import 'package:bookie_buddy_web/features/booking_details/view_model/cubit_booking_details_payment_history/booking_details_payment_history_cubit.dart';
 import 'package:bookie_buddy_web/features/completed_bookings/view_model/bloc_completed_bookings/completed_bookings_bloc.dart';
 import 'package:bookie_buddy_web/features/home/view_model/bloc_dashboard/dashboard_bloc.dart';
 import 'package:bookie_buddy_web/features/product/view_model/bloc_product/product_bloc.dart';
 import 'package:bookie_buddy_web/features/product/view_model/cubit_save_product/save_product_cubit.dart';
 import 'package:bookie_buddy_web/features/save_expense/view_model/cubit_save_expense/save_expense_cubit.dart';
-// import 'package:bookie_buddy_web/features/select_product_booking/view_model/bloc_select_product/select_product_bloc.dart';
-// import 'package:bookie_buddy_web/features/select_product_booking/view_model/cubit_selected_products/selected_products_cubit.dart';
+import 'package:bookie_buddy_web/features/select_product_booking/view/view_model/bloc_select_product/select_product_bloc.dart';
+import 'package:bookie_buddy_web/features/select_product_booking/view/view_model/cubit_selected_products/selected_products_cubit.dart';
 import 'package:bookie_buddy_web/features/splash/view/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -45,20 +48,25 @@ class MyApp extends StatelessWidget {
         BlocProvider(
             create: (context) =>
                 AddBookingBloc(bookingRepository: getIt.get())),
-        // BlocProvider(
-        //     create: (context) => SelectProductBloc(repository: getIt.get())),
+        BlocProvider(
+            create: (context) => SelectProductBloc(repository: getIt.get())),
         BlocProvider(
           create: (context) => ServiceBloc(repository: getIt.get())
             ..add(const ServiceEvent.loadServices()),
         ),
-        // BlocProvider(create: (context) => SelectedProductsCubit()),
+        BlocProvider(create: (context) => SelectedProductsCubit()),
         BlocProvider(
           create: (context) => DashboardBloc()
             ..add(
               const DashboardEvent.loadDashboardData(),
             ),
         ),
+        BlocProvider(
+            create: (context) =>
+                BookingDetailsPaymentHistoryCubit(repository: getIt.get())),
         BlocProvider(create: (context) => ProductBloc()),
+        BlocProvider(
+            create: (context) => StaffSearchCubit(repository: getIt.get())),
         BlocProvider(create: (context) => SaveProductCubit()),
         BlocProvider(create: (context) => BookingSelectionCubit()),
         BlocProvider(
@@ -68,7 +76,8 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => ClientCubit(repository: getIt.get())),
         BlocProvider(
             create: (context) => ClientSaveBloc(repository: getIt.get())),
-        // BlocProvider(create: (context) => ProductSearchBloc()),
+        BlocProvider(
+            create: (context) => ProductSearchCubit(repository: getIt.get())),
       ],
       child: MaterialApp(
         title: 'BookieBuddy',
