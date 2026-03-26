724bfb850789a1a4f3810599aecfc613551c59aa
AFNAN P <116930486+Afnanappu@users.noreply.github.com>
2026-03-26 11:04:48 +0530

    refactor(booking ui): removed unwanted files

--- lib/features/booking/presentation/booking_details/widgets/pdf_print_helper.dart
+++
@@ -1,3 +0,0 @@
-// Conditional export
-export 'pdf_print_helper_stub.dart'
-    if (dart.library.html) 'pdf_print_helper_web.dart';
--- lib/features/booking/presentation/booking_details/widgets/pdf_print_helper_stub.dart
+++
@@ -1,6 +0,0 @@
-// Stub implementation for non-web platforms
-import 'dart:typed_data';
-
-Future<void> printPdfWeb(Uint8List pdfBytes) async {
-  throw UnsupportedError('Printing is only supported on web platform');
-}
