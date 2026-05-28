19b1796a7c291d2fb38df88d0f6e20a5207a1aa3
SAYED ADINAN <147618795+sayedadinan@users.noreply.github.com>
2026-05-28 18:59:27 +0530

    feat: made the all needed changes on this

--- lib/features/sales/presentation/bloc/sales_details_bloc/sales_details_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'sales_details_bloc.dart';
-
-@freezed
-abstract class SalesDetailsEvent with _$SalesDetailsEvent {
-  const factory SalesDetailsEvent.fetchSaleDetails(int saleId) =
-      _FetchSaleDetails;
-
-  const factory SalesDetailsEvent.deleteSale(int saleId) = _DeleteSale;
-}
